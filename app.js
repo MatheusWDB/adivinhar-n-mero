9dc779fccc5a182e09f3e073cf1a3cde9eaaf398
MatheusWDB <matheuswdb1703@gmail.com>
2024-11-13 09:09:55 -0300

    a

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ let numeroMin = 1;
 
 alert(`Tente adivinhar o número secreto entre 1 e ${numeroMax}.`);
 
-let numeroSecreto = parseInt(Math.random() * numeroMax + 1) ;
+let numeroSecreto = parseInt(Math.random() * numeroMax + 1);
 let numeroEscolhido;
 let numeroTentativas = 1;
 
-do {
+while (numeroEscolhido != numeroSecreto) {
     numeroEscolhido = prompt("Escolha um numero.");
 
     if (numeroEscolhido < numeroSecreto) {
@@ -18,9 +18,9 @@ do {
     } else if (numeroEscolhido > numeroSecreto) {
         numeroTentativas++;
         alert("O número secreto é menor.");
-    } else {
+    } else if (numeroEscolhido == numeroSecreto){
         break;
     }
-} while (numeroEscolhido != numeroSecreto)
+} 
 
 alert(`Parabéns! Você acertou o número secreto: ${numeroSecreto}, com ${numeroTentativas} tentativa(s)`);
