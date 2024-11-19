4c9a95210d16bb8d3cf050333e3657954199aafe
MatheusWDB <matheuswdb1703@gmail.com>
2024-11-19 08:53:09 -0300

    refatoração do código

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
 alert("Seja bem vindo!")
 
-let numeroMax = 100;
+let numeroMax = 10;
 let numeroMin = 1;
 
 alert(`Tente adivinhar o número secreto entre 1 e ${numeroMax}.`);
 
-let numeroSecreto = parseInt(Math.random() * numeroMax + 1) ;
-let numeroEscolhido;
-let numeroTentativas = 1;
+let numeroSecreto = parseInt(Math.random() * numeroMax + 1);
+console.log(numeroSecreto);
 
-do {
-    numeroEscolhido = prompt("Escolha um numero.");
+let numeroEscolhido;
+let numeroTentativas = 0;
+
+while (numeroEscolhido != numeroSecreto) {
+    numeroEscolhido = parseInt(prompt("Escolha um numero."));
+    numeroTentativas++;
 
     if (numeroEscolhido < numeroSecreto) {
         numeroTentativas++;
         alert("O número secreto é maior.");
     } else if (numeroEscolhido > numeroSecreto) {
-        numeroTentativas++;
         alert("O número secreto é menor.");
-    } else {
+    } else if (numeroEscolhido == numeroSecreto){
         break;
+    } else {
+        alert("Isso nem é um número.");
     }
-} while (numeroEscolhido != numeroSecreto)
+}
 
 alert(`Parabéns! Você acertou o número secreto: ${numeroSecreto}, com ${numeroTentativas} tentativa(s)`);
