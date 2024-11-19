b08dc30360ef2b6868336e71636997951adc2961
MatheusWDB <matheuswdb1703@gmail.com>
2024-11-19 09:00:08 -0300

    consertando mesclagem

--- app.js
+++ app.js
@@ -1,41 +1,25 @@
 alert("Seja bem vindo!")
 
 let numeroMax = 10;
-<<<<<<< HEAD
 let numeroMin = 1;
-=======
->>>>>>> 8d5907f7d22c229ee7d948d20bdac972ce39cbc6
+let numeroEscolhido;
+let numeroTentativas = 0;
 
 alert(`Tente adivinhar o número secreto entre 1 e ${numeroMax}.`);
 
 let numeroSecreto = parseInt(Math.random() * numeroMax + 1);
 console.log(numeroSecreto);
-<<<<<<< HEAD
-
-let numeroEscolhido;
-let numeroTentativas = 0;
-
-while (numeroEscolhido != numeroSecreto) {
-    numeroEscolhido = parseInt(prompt("Escolha um numero."));
-    numeroTentativas++;
-=======
-let numeroEscolhido;
-let numeroTentativas = 0;
 
 while (numeroEscolhido != numeroSecreto) {
     numeroTentativas++;
-    numeroEscolhido = prompt("Escolha um numero.");
->>>>>>> 8d5907f7d22c229ee7d948d20bdac972ce39cbc6
+    numeroEscolhido = prompt("Escolha um número.");
 
     if (numeroEscolhido < numeroSecreto) {
         alert("O número secreto é maior.");
     } else if (numeroEscolhido > numeroSecreto) {
         alert("O número secreto é menor.");
-<<<<<<< HEAD
-    } else if (numeroEscolhido == numeroSecreto){
-=======
     } else if (numeroEscolhido == numeroSecreto) {
->>>>>>> 8d5907f7d22c229ee7d948d20bdac972ce39cbc6
+    } else if (numeroEscolhido == numeroSecreto) {
         break;
     } else {
         alert("Isso nem é um número.");
